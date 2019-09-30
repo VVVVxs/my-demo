3bda36beaf9381409469bc1cbb7236646af28315
VVVVxs <285134765@qq.com>
2019-09-30 22:34:01 +0800

    轮播滚动优化！

--- fe/src/component/home/componets/banner.tsx
+++ fe/src/component/home/componets/banner.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 const { useEffect, useState } = React;
+
 const Banner: React.FC = () => {
+    const images: string[] = [
+        '../../../../static/images/1.jpg',
+        '../../../../static/images/2.jpg',
+        '../../../../static/images/3.jpg'
+    ]
     const [marginLeft, setMarginLeft] = useState(0);
+    const [active, setActive] = useState(0);
     useEffect(() => {
         timeer()
     }, [])
@@ -10,30 +17,48 @@ const Banner: React.FC = () => {
         setInterval(() => {
             setMarginLeft((marginLeft: number) => {
                 let newMarginLft = marginLeft;
-                if (newMarginLft === -2200) {
+                if (newMarginLft === -2100) {
                     newMarginLft = 0;
+                    setActive(0);
                 } else {
-                    newMarginLft = newMarginLft - 5;
+                    newMarginLft = newMarginLft - 1;
+                    if(newMarginLft===-700){
+                        setActive(1)
+                    }
+                    
+                    if(newMarginLft===-1400){
+                        setActive(2)
+                    }
                 }
 
                 return newMarginLft;
 
             });
-        }, 50);
+        }, 10);
     }
 
-    console.log(marginLeft)
-
     return (
-        <div className='banner'>
-            <ul className='bannerContainer' style={{ marginLeft: marginLeft }}>
-                <li><img src="../../../../static/images/1.jpg" alt="" /></li>
-                <li><img src="../../../../static/images/2.jpg" alt="" /></li>
-                <li><img src="../../../../static/images/3.jpg" alt="" /></li>
-                <li><img src="../../../../static/images/1.jpg" alt="" /></li>
-            </ul>
+        <div className='bannerMain'>
+            <div className='banner'>
+                <ul className='bannerContainer' style={{ marginLeft: marginLeft }}>
+                    {
+                        images.map((imgUrl: string, index: number) => {
+                            return (<li key={index}><img src={imgUrl} alt="" /></li>)
+                        })
 
+                    }
+                    <li key={images.length + 1}><img src={images[0]} alt="" /></li>
+                </ul>
+            </div>
+            <ul className="doc">
+                {
+                    images.map((images: string, index: number) => {
+                        return (<li key={index} style={{ background: active === index ? '#999' : 'transparent' }}></li>)
+                    })
+                }
+            </ul>
         </div>
+
     )
 }
 export default Banner;
